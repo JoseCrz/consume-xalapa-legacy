15b181a8b9fa27cd40e9a4b39c2c333767503c38
Jose Cuevas <josecueram@gmail.com>
2020-04-03 01:38:04 -0600

    implemented multer to recieve form

--- components/restaurant/network.js
+++ components/restaurant/network.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const multer = require('multer')
 const response = require('../../network/response')
 const controller = require('./controller')
 
 const router = express.Router()
+
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/img/')
+    },
+    filename: (req, file, cb) => {
+        const splitedName = file.originalname.split('.')
+        const extension = splitedName[splitedName.length - 1]
+        cb(null, `${Date.now()}.${extension}`)
+    }
+})
+
+const upload = multer({
+    storage: multerStorage
+})
 
 router.get('/', (req, res) => {
     controller.getRestaurants({})
@@ -20,17 +36,19 @@ router.get('/add', (req, res) => {
     res.render('addRestaurants.ejs')
 })
 
-router.post('/', (req, res) => {
-    const restaurant = req.body
+router.post('/', upload.single('image') ,(req, res) => {
+    console.log(req.file)
+    console.log(req.body)
+    // const restaurant = req.body
     // console.log(restaurant)
-    controller.addRestaurant(restaurant)
-        .then(addedRestaurant => {
-            response.success(req, res, addedRestaurant, 201)
-        })
-        .catch(error => {
-            console.log(error)
-            response.error(req, res, 'Something went horrible wrong :(', 500)
-        })
+    // controller.addRestaurant(restaurant)
+    //     .then(addedRestaurant => {
+    //         response.success(req, res, addedRestaurant, 201)
+    //     })
+    //     .catch(error => {
+    //         console.log(error)
+    //         response.error(req, res, 'Something went horrible wrong :(', 500)
+    //     })
 })
 
 module.exports = router
