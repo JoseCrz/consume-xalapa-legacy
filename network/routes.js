ae1d37722c04d2fcfb5674841edf38157520533b
Jose Cuevas <josecueram@gmail.com>
2020-04-14 13:09:54 -0500

    added map routes and views

--- network/routes.js
+++ network/routes.js
@@ -2,12 +2,14 @@ const landingRoutes = require('../components/landing/network')
 const restaurantRoutes = require('../components/restaurant/network')
 const categoryRoutes = require('../components/category/network')
 const mapRoutes = require('../components/map/network')
+const promoRoutes = require('../components/promo/network')
 
 const routes = app => {
     app.use('/', landingRoutes)
     app.use('/restaurants', restaurantRoutes)
     app.use('/category', categoryRoutes)
     app.use('/mapa', mapRoutes)
+    app.use('/promos', promoRoutes)
 }
 
 module.exports = routes
